cc7b70fe6e9689ced6f6e36c835f318a41e06bd3
Toby Hede <tobyhede@info-architects.net>
2016-08-04 11:27:39 +1000

    Adjust ‘spec’ to not break
    Its not a spec,

--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Dashboard, type: :model do
 
-  let(:agency) { 'Dept of Vtha' }
+  let(:agency) { Agency.create!(:name => 'Dept of Vtha', :url => 'vtha.gov.au')  }
 
   it 'is not a good test' do
 
-    dashboard = Dashboard.create!(:name => agency, :agency => agency)
+    dashboard = Dashboard.create!(:name => 'Dashboard', :agency => agency)
 
     widget = Widget.create!(
+      :dashboard    => dashboard,
       :name         => 'numbers-bar',
       :description  => 'numbers',
       :size         => 'small',
       :type         => 'line',
-      :units        => 'n')
+      :units        => 'n',
+      :row          => 0,
+      :pos          => 0)
 
     dataset = widget.datasets.create!(
       :name => 'numbers',
