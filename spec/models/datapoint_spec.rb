2f11e4f3f3c69e2942d3b52d34c23befabf5779f
Toby Hede <tobyhede@info-architects.net>
2016-08-08 16:46:30 +1000

    Data point label spec

--- spec/models/datapoint_spec.rb
+++ spec/models/datapoint_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Datapoint, type: :model do
   it { is_expected.to validate_presence_of :ts }
   it { is_expected.to validate_numericality_of :value }
 
+  let(:dataset) { Fabricate(:dataset) }
+
+  describe '#label' do
+    subject     { Fabricate(:datapoint, :ts => Date.parse('2016-01-01'), :dataset => dataset) }
+    its(:label) { is_expected.to eq '2016-01' }
+  end
+
 end
--- spec/models/dataset_spec.rb
+++ spec/models/dataset_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Dataset, type: :model do
-  
+
   it { is_expected.to belong_to :organisation }
   it { is_expected.to have_many :widgets }
   it { is_expected.to have_many :datapoints }
   it { is_expected.to have_many :dataset_widgets }
 
   it { is_expected.to validate_presence_of :name }
+
 
 end
