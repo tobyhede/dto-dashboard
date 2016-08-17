140005caedf6ed27896b7534b5643e68f533b047
elisechant <elise.chant@gmail.com>
2016-08-18 08:59:16 +1000

    adds body class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 
-  def body_class(class_name="poo")
-    content_for :body_class, class_name
+  # Sets the body class on a per-page basis
+  def body_class(class_names)
+    class_name_str = self.controller_name + '_' + self.action_name
+    if !class_names.empty?
+      class_name_str << ' ' + class_names
+    end
+    return class_name_str
   end
 
 end
