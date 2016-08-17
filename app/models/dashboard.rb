81b37e1a6666220c4937dc4c8bb58c055d8ce372
Toby Hede <tobyhede@info-architects.net>
2016-08-17 12:16:25 +1000

    Index with KPI widgets

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -4,6 +4,8 @@ class Dashboard < ApplicationRecord
   belongs_to :organisation
   has_many :widgets
 
-  scope :published, -> { where('published_at <= NOW()') }
+  def self.published
+    where('published_at <= NOW()')
+  end
 
 end
