3240942986dacbdc35097b57428ae40c87cb0619
Toby Hede <tobyhede@info-architects.net>
2016-08-18 12:43:53 +1000

    DashboardDecorator  and helper methods

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -1,4 +1,20 @@
 class DashboardDecorator < Draper::Decorator
   delegate_all
 
+  def show_hero?
+    display_hero && display_kpis && widgets.hero.present?
+  end
+
+  def show_kpis?
+    display_kpis && widgets.kpis.any?
+  end
+
+  def show_notes?
+    notes.present?
+  end
+
+  def show_url?
+    url.present?
+  end
+
 end
