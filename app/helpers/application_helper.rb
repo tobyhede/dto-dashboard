292496fa9244fb5f273221e2fcc129bd25f74e6e
elisechant <elise.chant@gmail.com>
2016-08-18 11:07:32 +1000

    fix high contrast mode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
 
   # Sets the body class on a per-page basis
+  # Params:
+  # - class_names: String of class names to append to class names string
   def body_class(class_names)
     class_name_str = self.controller_name + '_' + self.action_name
     if !class_names.empty?
