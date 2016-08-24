bafaa83dc859bd03127d631bdc475f8dece94a49
gortonz <gwzhao@yahoo.com>
2016-08-25 08:45:36 +1000

    Use FactoryGirl to create a test dashboard data

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -13,15 +13,16 @@ RSpec.describe DashboardsController, :type => :controller do
   end
 
   # Please uncomment the following test when the test data is imported to the test database
-  # describe "GET show page with a valid dashboard id" do
-  #   it "responds successfully with an HTTP 200 status code" do
-  #     get :show, params: {id: 1}
-  #     expect(response).to be_success
-  #     expect(response).to have_http_status(200)
-  #     expect(response).to render_template(:show)
-  #   end
-  #
-  # end
+  describe "GET show page with a valid dashboard id" do
+    let(:dashboard)    { FactoryGirl.create(:dashboard) }
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, params: {id: dashboard.id}
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+    end
+
+  end
 
   describe "GET show page with an invalid dashboard id" do
     it "should catch an exception" do
