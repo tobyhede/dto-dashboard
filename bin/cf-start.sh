3e0bdd288664fdca56d7e52ed526a7de71aee260
Toby Hede <tobyhede@info-architects.net>
2016-08-26 12:16:24 +1000

    CF does not own the database

--- bin/cf-start.sh
+++ bin/cf-start.sh
@@ -1,4 +1,4 @@
 npm install
 npm run build
-RAILS_ENV=$RAILS_ENV bundle exec rake db:drop db:create db:migrate import:data
+RAILS_ENV=$RAILS_ENV bundle exec rake db:schema:load import:data
 RAILS_ENV=$RAILS_ENV bundle exec rake cf:on_first_instance db:migrate && exec bundle exec rails s -p $PORT -e $RAILS_ENV
