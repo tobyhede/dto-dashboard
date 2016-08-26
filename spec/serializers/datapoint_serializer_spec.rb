f1b9148f67f879ac740647813fbfe29fed46b54c
Toby Hede <tobyhede@info-architects.net>
2016-08-26 11:32:01 +1000

    Fix datapoint validation error in spec

--- spec/serializers/datapoint_serializer_spec.rb
+++ spec/serializers/datapoint_serializer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe DatapointSerializer, type: :serializer do
 
-  let(:datapoint) { FactoryGirl.create(:datapoint, :dataset_id => 1) }
+  let(:dataset) { FactoryGirl.create(:dataset) }
+  let(:datapoint) { FactoryGirl.create(:datapoint, :dataset => dataset) }
 
   let(:serializer)  { DatapointSerializer.new(datapoint).to_hash }
 
