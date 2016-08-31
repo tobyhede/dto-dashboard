dae09aa0d8aea507714437db3840fd2ceaa71593
Dave Cheney <dave@cheney.net>
2016-09-01 09:01:45 +1000

    Update cf-start.sh
    Clean up bin/cf-start.sh

--- bin/cf-start.sh
+++ bin/cf-start.sh
@@ -1,4 +1,8 @@
+#!/bin/bash 
+
+set -xeu
+
 npm install
 npm run build
-RAILS_ENV=$RAILS_ENV bundle exec rake db:schema:load import:data
-RAILS_ENV=$RAILS_ENV bundle exec rake cf:on_first_instance db:migrate && exec bundle exec rails s -p $PORT -e $RAILS_ENV
+bundle exec rake db:schema:load import:data
+bundle exec rake cf:on_first_instance db:migrate && exec bundle exec rails s -p $PORT -e $RAILS_ENV
