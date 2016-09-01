2267d279ffb6efb901dd39fdcf6ebd5d65d2435f
Toby Hede <tobyhede@info-architects.net>
2016-09-01 13:25:43 +1000

    Build assets in circle deployment

--- bin/cf-start.sh
+++ bin/cf-start.sh
@@ -1,8 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -xeu
 
-npm install
-npm run build
 bundle exec rake db:schema:load import:data
 bundle exec rake cf:on_first_instance db:migrate && exec bundle exec rails s -p $PORT -e $RAILS_ENV
