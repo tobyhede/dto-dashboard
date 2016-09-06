6d5606356058b5190bb5d059a0a26fcb35e738e1
Toby Hede <tobyhede@info-architects.net>
2016-09-06 16:14:22 +1000

    Remove data import from cf start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD'] if ENV['HTTP_BASIC_AUTH']
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD'] if (Rails.env.cloudfoundry? || Rails.env.staging?)
 
   protect_from_forgery with: :exception
 
