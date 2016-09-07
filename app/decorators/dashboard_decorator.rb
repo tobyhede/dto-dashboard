b927e85d212cfcb092c32a121aec264c66ba70b2
gortonz <gwzhao@yahoo.com>
2016-09-08 09:18:53 +1000

    Outputed dashboard data in csv format

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -37,7 +37,45 @@ class DashboardDecorator < Draper::Decorator
     object.last_updated_at.to_formatted_s(:month_year)
   end
 
+  def to_csv
+    cnt = CSV.generate( {} ) do |csv|
+      csv.add_row CVS_COL_NAME
+
+      dashboard.first_row.each do |kpi|
+        widget = kpi.decorate
+
+        wi_values ||= []
+        wi_values.push(widget.id || " ")
+        wi_values.push(widget.name || " ")
+        wi_values.push(widget.type || " ")
+        wi_values.push(widget.units || " ")
+        wi_values.push(widget.description || " ")
+        wi_values.push(widget.options || " ")
+
+        widget.datasets.each do |dataset|
+          if dataset.datapoints != nil
+            dataset.datapoints.each do |datapoint|
+              dp_values ||= []
+              dp_values.push(datapoint.label() || " ")
+              dp_values.push(datapoint.value || " ")
+              csv.add_row wi_values + dp_values
+            end
+          else
+            dp_values ||=[" ", " "]
+            csv.add_row wi_values + dp_values
+          end
+        end
+
+      end
+
+    end
+
+    puts cnt.to_s
+    return cnt
+  end
+
   private
+  CVS_COL_NAME = ['id', 'last_updated_at', 'name','type', 'units','description', 'options', 'data_label', 'data_value']
 
   def markdown
     render_options = {
--- spec/decorators/dashboard_decorator_spec.rb
+++ spec/decorators/dashboard_decorator_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DashboardDecorator, type: :decorator do
-  let(:opts)      { {} }
+  let(:opts) { {} }
   let(:dashboard) { FactoryGirl.create(:dashboard_with_widgets, opts) }
   let(:decorator) { DashboardDecorator.new(dashboard) }
 
-  subject {dashboard.decorate}
+  subject { dashboard.decorate }
 
   context 'dashboard without widgets' do
     let(:dashboard) { FactoryGirl.create(:dashboard, opts) }
@@ -19,20 +19,20 @@ RSpec.describe DashboardDecorator, type: :decorator do
   end
 
   context 'hide hero' do
-    let(:opts) { { :display_hero => false } }
+    let(:opts) { {:display_hero => false} }
     it { is_expected.to_not be_show_hero }
     it { is_expected.to be_show_kpis }
   end
 
   context 'hide kpis' do
-    let(:opts) { { :display_hero => false, :display_kpis => false } }
+    let(:opts) { {:display_hero => false, :display_kpis => false} }
     it { is_expected.to_not be_show_hero }
     it { is_expected.to_not be_show_kpis }
   end
 
 
   describe 'convert notes to html' do
-    let(:opts) { { :notes => '# Heading' } }
+    let(:opts) { {:notes => '# Heading'} }
 
     subject { decorator.notes_to_html }
 
@@ -41,7 +41,7 @@ RSpec.describe DashboardDecorator, type: :decorator do
   end
 
   describe 'title' do
-    let(:opts) { { :name => 'Blah Dashboard' } }
+    let(:opts) { {:name => 'Blah Dashboard'} }
     subject { decorator.name }
 
     it { is_expected.to eq 'Blah' }
@@ -49,13 +49,13 @@ RSpec.describe DashboardDecorator, type: :decorator do
 
   describe 'dashboardized_name' do
     context 'dashboard' do
-      let(:opts) { { :name => 'Blah Dashboard' } }
+      let(:opts) { {:name => 'Blah Dashboard'} }
       subject { decorator.dashboardized_name }
 
       it { is_expected.to eq 'Blah Dashboard' }
     end
     context 'no dashboard' do
-      let(:opts) { { :name => 'Blah' } }
+      let(:opts) { {:name => 'Blah'} }
       subject { decorator.dashboardized_name }
 
       it { is_expected.to eq 'Blah Dashboard' }
@@ -65,5 +65,12 @@ RSpec.describe DashboardDecorator, type: :decorator do
   describe 'last_updated_at' do
     subject { decorator.last_updated_at }
     it { is_expected.to eq Time.zone.now.to_formatted_s(:month_year) }
+  end
+
+  describe 'to_csv' do
+    context 'csv content not empty' do
+      subject { decorator.to_csv }
+      it { is_expected.not_to be_empty }
+    end
   end
 end
