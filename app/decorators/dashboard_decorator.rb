a65141dbe9310eaaff8e16868c1563756805a698
gortonz <gwzhao@yahoo.com>
2016-09-08 15:10:34 +1000

    Download dashboard data as the csv file

--- app/decorators/dashboard_decorator.rb
+++ app/decorators/dashboard_decorator.rb
@@ -38,76 +38,28 @@ class DashboardDecorator < Draper::Decorator
   end
 
   def to_csv
-    CSV.generate( {} ) do |csv|
+    CSV.generate({}) do |csv|
       csv.add_row CVS_COL_NAME
 
-      # export KPI data
-      self.first_row.each do |kpi|
-        widget = kpi.decorate
+      self.widgets.each do |widget|
+        dataset = widget.dataset
+        if !dataset.nil? && !dataset.datapoints.nil?
+          darray = dataset.datapoints.collect { |d| [dataset.name, d.ts.strftime("%d/%m/%Y %H:%M"), d.value.to_s] }
 
-        kpi_values ||= []
-        kpi_values.push(widget.id || " ")
-        kpi_values.push(widget.name || " ")
-        kpi_values.push(widget.type || " ")
-        kpi_values.push(widget.size || " ")
-        kpi_values.push(widget.units || " ")
-        kpi_values.push(widget.description || " ")
-        kpi_values.push(widget.options || " ")
-
-        widget.datasets.each do |dataset|
-          if dataset.datapoints != nil
-            dataset.datapoints.each do |datapoint|
-              dp_values ||= []
-              dp_values.push(datapoint.label() || " ")
-              dp_values.push(datapoint.value || " ")
-              csv.add_row kpi_values + dp_values
+          if darray.any?
+            darray.each do |item|
+              csv.add_row item
             end
-          else
-            dp_values ||=[" ", " "]
-            csv.add_row kpi_values + dp_values
           end
+
         end
-
       end
-
-      # export other wiget data
-      self.remaining_rows.each do |other_widget|
-        widget = other_widget.decorate
-
-        wi_values ||= []
-        wi_values.push(widget.id || " ")
-        wi_values.push(widget.name || " ")
-        wi_values.push(widget.type || " ")
-        wi_values.push(widget.size || " ")
-        wi_values.push(widget.units || " ")
-        wi_values.push(widget.description || " ")
-        wi_values.push(widget.options || " ")
-
-        widget.datasets.each do |dataset|
-          if dataset.datapoints != nil
-            dataset.datapoints.each do |datapoint|
-              dp_values ||= []
-              dp_values.push(datapoint.label() || " ")
-              dp_values.push(datapoint.value || " ")
-              csv.add_row wi_values + dp_values
-            end
-          else
-            dp_values ||=[" ", " "]
-            csv.add_row wi_values + dp_values
-          end
-        end
-
-      end
-
     end
 
-    # uncomment following for debugging only
-    # puts cnt.to_s
-    # return cnt
   end
 
   private
-  CVS_COL_NAME = ['id', 'last_updated_at', 'name','type','size','units','description', 'options', 'data_label', 'data_value']
+  CVS_COL_NAME = ['dataset_name', 'time_stamp', 'data_value']
 
   def markdown
     render_options = {
