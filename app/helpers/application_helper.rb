4e57340098f4bf97681055f08dadf216d02a93c1
elisechant <elise.chant@gmail.com>
2016-09-09 12:37:40 +1000

    update helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,11 @@ module ApplicationHelper
 
   # Sets the body class on a per-page basis
   # Params:
-  # - class_names: Array of additional class names to append to body class
+  # - class_names: {Array} Array of additional class names to append to body class
   # names
   def body_class(names)
-    class_names = [self.controller_name + '_' + self.action_name]
-    if !names.empty?
-      class_names.push(names);
-    end
-    return class_names.join(' ');
+    class_names = ["#{controller_name}_#{action_name}"] << names
+    class_names.flatten.join(' ')
   end
 
 end
