6ad77942f4488579a994fd6f502e64864274f695
gortonz <gwzhao@yahoo.com>
2016-09-14 17:07:20 +1000

    A minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
 
   http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD'] if (Rails.env.cloudfoundry? || Rails.env.staging?)
 
+  protect_from_forgery with: :exception
+
 end
