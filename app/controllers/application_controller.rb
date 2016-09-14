43f96cc908d648ef4522af2cdf21d98b29c85554
gortonz <gwzhao@yahoo.com>
2016-09-14 16:46:48 +1000

    Removed one sign up code and deleted ominauth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
 
   end
