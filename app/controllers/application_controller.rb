058a5516b5b04dc3a1f7d67a7a6ca295cc5c7692
gortonz <gwzhao@yahoo.com>
2016-09-14 16:38:22 +1000

    Uncommented out the HTTP basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Sslify
 
-  #http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD'] if (Rails.env.cloudfoundry? || Rails.env.staging?)
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD'] if (Rails.env.cloudfoundry? || Rails.env.staging?)
 
   # The following several lines should be moved into the controllers need to be authenticated
   protect_from_forgery with: :exception
