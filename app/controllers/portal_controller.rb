6c47e4211885cd0feb603ba58203379130b976d5
gortonz <gwzhao@yahoo.com>
2016-09-15 09:32:36 +1000

    Removed a few unwanted controllers and config parameters

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,12 +1,11 @@
 class PortalController < ApplicationController
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: []
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
 
   end
 
