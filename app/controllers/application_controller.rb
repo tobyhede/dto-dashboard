a7e5959da0b7cdb35e5e24d2331369e8c611a766
Toby Hede <tobyhede@info-architects.net>
2016-09-16 09:24:26 +1000

    Redirect to portal on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_filter :store_current_location, :unless => :devise_controller?
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+   '/portal'
+  end
+
 end
