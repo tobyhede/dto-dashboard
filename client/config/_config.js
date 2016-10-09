1b51f3b4c023940e72932a487010a45f485fb0a1
Elise Chant <elise.chant@gmail.com>
2016-10-10 07:34:01 +1100

    be explicit about host and port that webpack owns (#232)

--- client/config/_config.js
+++ client/config/_config.js
@@ -1,8 +1,8 @@
 import path from 'path';
 
 export const ENV = process.env.NODE_ENV || 'development';
-export const HOST = 'localhost';
-export const PORT = 8080;
+export const WEBPACK_HOST = 'localhost';
+export const WEBPACK_PORT = 8080;
 export const DIR_ROOT = path.resolve('./');
 export const DIR_SRC = path.join(DIR_ROOT, '/client/src');
 export const DIR_TEST = path.join(DIR_ROOT, '/client/test');
