65d52f29bef2d62fad4670031c89fdee385cea76
Elise Chant <elise.chant@gmail.com>
2016-10-13 12:12:29 +1100

    hotfixes - browser issues fixes (#250)
    * fix background blue issue (#242)
    
    * fix no logo issue on ie9 (#244)
    
    * fix footer bottom space issue (#243)
    
    * change min-height of widget; (#248)
    
    * remove outline on logo (#245)
    
    * remove outline on logo
    
    * actually fix
    
    * fixes fact text overflow issue ie10 (#246)
    
    * removes heights on fact title; inline style for fact description text;
    
    * remove padding bottom
    
    * Hotfix/pd 644 fix card layout on tablet (#249)
    
    * fix show view kpi’s
    
    * fix sizing on show page
    
    * fix type size and decorator alignment
    
    * fix missing metrics
    
    * patch logo
    
    * remove console.log; fix fact height on small screens;
    
    * fix consolelog thingo
    
    * Hotfix/tablet kpi (#254)
    
    * fix breakpoint 767
    
    * fix widget titles
    
    * remove commented out code
    
    * set a width of logo at desktop

--- client/src/dashboard/legacy/Widgets/FactWidget.js
+++ client/src/dashboard/legacy/Widgets/FactWidget.js
@@ -1,12 +1,15 @@
+/**
+ * Need to style instead thanks to an overflow issue with ie10
+ */
 class FactWidget {
   constructor(options){
-    this.container = options.element.append('div').attr('class', 'fact__inner');
+    this.container = options.element.append('div').attr('class', 'fact__inner').attr('style', 'width:100%;height:100%;');
     this.description = options.description || 'no fact available';
     this.init();
   }
 
   init(){
-    this.container.append('p').text(this.description);
+    this.container.append('p').attr('style', 'display:block;width:100%;height:100%;').text(this.description);
   }
 }
 
